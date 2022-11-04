e8ec565a366a818d6a2f32452803b29009aeeddf
ws <hunt_hak@outlook.com>
2022-11-04 15:55:41 +0800

    add multiprocessing

--- recursive_imreg/utils/dataloads.py
+++ recursive_imreg/utils/dataloads.py
@@ -5,6 +5,8 @@ import numpy as np
 from torch.utils import data
 from functools import lru_cache
 
+from multiprocessing.dummy import Pool
+
 sys.path.append("../")
 sys.path.append("./")
 from utils.base_util import getImageDirs, loadJson, readNiiImage, saveNiiImage
@@ -22,7 +24,8 @@ class DatasetPtCrop(data.Dataset):
 
     def __getitem__(self, index):
         # imgA_crop, imgB_crop, filename = self.datas[index]
-        imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename = self.datas[index]
+        imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename = self.datas[
+            index]
 
         imgA_tensor = torch.FloatTensor(imgA_crop).unsqueeze(0)
         imgB_tensor = torch.FloatTensor(imgB_crop).unsqueeze(0)
@@ -88,36 +91,57 @@ def processMulData(file, r, limit):
     return imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename
 
 
-def getDatas(root_dir):
+def getDatas(root_dir, pool_size=12):
     train_datas = []
     test_datas = []
 
-    r = loadJson("files/train_coordinate.json")
-    # limit = [200, -60, 128, 128, 128, 128]
-    limit = [256, -60, 200, 128, 128, 128]
-
     image_dirs = getImageDirs(root_dir)
 
-    pre_images, suf_images = image_dirs[:147] + image_dirs[153:], image_dirs[
-        147:153]
-    X_train_imgs = suf_images[:-1]
-    X_test_imgs = suf_images[-1:]
+    if "train" in root_dir:
+        r = loadJson("files/train_coordinate.json")
+        pre_images, suf_images = image_dirs[:147] + image_dirs[
+            153:], image_dirs[147:153]
+        X_train_imgs = suf_images[:2]
+        X_test_imgs = suf_images[2:]
+    elif "test" in root_dir:
+        r = loadJson("files/train_coordinate.json")
+        X_train_imgs = image_dirs[:36]
+        X_test_imgs = image_dirs[36:]
+    else:
+        raise NotImplementedError(f"The file {root_dir} doesn't exist!")
+    limit = [256, -60, 200, 128, 128, 128]
 
+    pool = Pool(pool_size)
+    train_res = []
     for i in range(len(X_train_imgs)):
-        # imgA_crop, imgB_crop, filename = processData(X_train_imgs[i], r, limit)
-        # train_datas.append((imgA_crop, imgB_crop, filename))
-        imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename = processMulData(
-            X_train_imgs[i], r, limit)
-        train_datas.append(
-            (imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename))
+        train_temp = pool.apply_async(processMulData,
+                                      (X_train_imgs[i], r, limit))
+        train_res.append(train_temp)
+    pool.close()
+    pool.join()
 
+    for train_r in train_res:
+        train_datas.append(train_r.get())
+    #     imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename = processMulData(
+    #         X_train_imgs[i], r, limit)
+    #     train_datas.append(
+    #         (imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename))
+
+    pool = Pool(pool_size)
+    test_res = []
     for i in range(len(X_test_imgs)):
-        # imgA_crop, imgB_crop, filename = processData(X_test_imgs[i], r, limit)
-        # test_datas.append((imgA_crop, imgB_crop, filename))
-        imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename = processMulData(
-            X_train_imgs[i], r, limit)
-        test_datas.append(
-            (imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename))
+        test_temp = pool.apply_async(processMulData,
+                                     (X_test_imgs[i], r, limit))
+        test_res.append(test_temp)
+    pool.close()
+    pool.join()
+
+    for test_r in test_res:
+        test_datas.append(test_r.get())
+    #     imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename = processMulData(
+    #         X_test_imgs[i], r, limit)
+    #     test_datas.append(
+    #         (imgA_crop, imgB_crop, imgA_gt_crop, imgB_gt_crop, filename))
     return train_datas, test_datas
 
 
@@ -136,36 +160,8 @@ def getDataloader(root_dir):
     test_set = DatasetPtCrop(X_test)
     test_dataloader = data.DataLoader(test_set, **params_train)
     return train_dataloader, test_dataloader
-
-
-def getDataloaderTest(root_dir):
-    """ pt_mode: `norch` or `condyle` """
-    image_dirs = getImageDirs(root_dir)
-    pre_images = image_dirs
-    pre_images.remove(os.path.join(root_dir, "img46"))
-    pre_images.remove(os.path.join(root_dir, "img45"))
-
-    length = len(pre_images)
-    X_train = pre_images[:int(0.2 * length)] + pre_images[int(0.4 * length):]
-    X_test = pre_images[int(0.2 * length):int(0.4 * length)]
-
-    r = loadJson("part5/jsonfile/test_coordinate.json")
-    # limit = [200, -60, 128, 128, 128, 128]
-    limit = [256, -60, 200, 128, 128, 128]
-    params_train = {
-        'batch_size': conf.batch_size,
-        'shuffle': True,
-        'num_workers': conf.numwork,
-        'worker_init_fn': np.random.seed(1)
-    }
-    train_set = DatasetPtCrop(X_train, r=r, limit=limit)
-    train_dataloader = data.DataLoader(train_set, **params_train)
-    test_set = DatasetPtCrop(X_test, r=r, limit=limit)
-    test_dataloader = data.DataLoader(test_set, **params_train)
-    return train_dataloader, test_dataloader
+    """none"""
 
 
 if __name__ == "__main__":
-    """none"""
-    # a, b = getDataloaderTest(os.path.join(conf.root_path, "testdata"))
     a, b = getDataloader(os.path.join(conf.root_path, "traindata"))
